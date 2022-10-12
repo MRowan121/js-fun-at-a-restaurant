a18fd13ca322d40f93b3644e7c17db434f32f391
Matt Rowan <matt.rowan121@gmail.com>
2022-10-12 17:39:18 -0400

    Reduce instock inventory

--- src/warm-up.js
+++ src/warm-up.js
@@ -22,7 +22,8 @@ console.log(inStock);
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock = inStock - 1;
+console.log(inStock);
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
