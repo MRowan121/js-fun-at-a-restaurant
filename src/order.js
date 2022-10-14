2ad011d9f09f683b1c52d8efa99a2bff7abef05c
Matt Rowan <matt.rowan121@gmail.com>
2022-10-13 22:18:28 -0400

    Add takeorder function

--- src/order.js
+++ src/order.js
@@ -1,6 +1,14 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+    } else {
+    return;
+  };
+};
+
 
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
