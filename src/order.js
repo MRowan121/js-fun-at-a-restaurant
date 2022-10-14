d5be5a03654cacd893b7af79cd3dd3c10868cc96
Matt Rowan <matt.rowan121@gmail.com>
2022-10-14 17:30:38 -0400

    Add refund order function

--- src/order.js
+++ src/order.js
@@ -6,10 +6,20 @@ function takeOrder(order, deliveryOrders) {
   };
 };
 
+function refundOrder(order, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === order) {
+      return deliveryOrders.splice(i, 1);
+    };
+  };
+  
+  // if (deliveryOrders.includes(orderNumber)) {
+  //   return deliveryOrders.splice(deliveryOrders);
+};
 
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
