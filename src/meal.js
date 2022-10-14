c298dcd96e6d09fc00bcf1279e72593ee9b682f3
Matt Rowan <matt.rowan121@gmail.com>
2022-10-13 21:28:35 -0400

    Add meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,42 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  // console.log(`Delicious ${food}`);
+  return `Delicious ${food}`;
 }
 
+function createMenuItem(name, price, type) {
+  // console.log({name: name, price: price, type: type});
+  return {name: name, price: price, type: type};
+};
+
+
+function addIngredients(food, ingredients) {
+  // ingredients.push(food);
+  if (ingredients.includes(food)) {
+    return 
+    } else {
+    return ingredients.push(food);
+  };
+};
+
+function formatPrice(price) {
+  return price = `$${price}`;
+};
+
+function decreasePrice(reducePrice) {
+  return reducePrice - (reducePrice * .10);
+};
+
+function createRecipe(title, ingredients, type) {
+  return {title: title, ingredients: ingredients, type: type};
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
