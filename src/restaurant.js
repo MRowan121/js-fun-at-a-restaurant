957cf0422cc5c24559065b6cd6b40419a824fe9b
Matt Rowan <matt.rowan121@gmail.com>
2022-10-15 15:02:40 -0400

    Create add menu item function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,11 +1,22 @@
 
-function createRestaurant(name) {
+function createRestaurant(restaurant) {
   // console.log({name: name, menus: {breakfast: [], lunch: [], dinner: []}});
-  return {name: name, menus: {breakfast: [], lunch: [], dinner: []}};
+  return {name: restaurant, menus: {breakfast: [], lunch: [], dinner: []}};
 };
+
+function addMenuItem(restaurant, pizza) {
+  if (pizza.type === "breakfast" && !restaurant.menus.breakfast.includes(pizza)) {
+  return restaurant.menus.breakfast.push(pizza);
+  } else if (pizza.type === "lunch" && !restaurant.menus.lunch.includes(pizza)) {
+  return restaurant.menus.lunch.push(pizza);
+  } else if (pizza.type === "dinner" && !restaurant.menus.dinner.includes(pizza)) {
+  return restaurant.menus.dinner.push(pizza);
+  };
+};
+
 
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
