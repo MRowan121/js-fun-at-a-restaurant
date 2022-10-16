d2fd6593445163f1a4fcb97212f4feb6bf3c7be2
Matt Rowan <matt.rowan121@gmail.com>
2022-10-16 13:53:35 -0400

    Add function to greet customer

--- src/Chef.js
+++ src/Chef.js
@@ -2,7 +2,11 @@ class Chef {
   constructor(name, restaurant) {
     this.name = name;
     this.restaurant = restaurant;
-  }
-}
+  };
+
+  greetCustomer(customer) {
+    return `Hello, ${customer}!`
+  };
+};
 
 module.exports = Chef;
